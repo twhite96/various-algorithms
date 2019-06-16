94e25f188fa12c6ff6420a2904b13b4430959e6b
Tiffany White <hey@tiffanyrwhite.com>
2019-06-15 21:24:44 -0400

    Update something

---
+++ codesignal/adjacent-element-product.js
@@ -0,0 +1,6 @@
+function adjacentElementsProduct(inputArray) {
+ // use divide and conquer algorithm;
+  // start in the middle and compare one side of the array
+  // to its peak or the largest number in the array
+
+}
